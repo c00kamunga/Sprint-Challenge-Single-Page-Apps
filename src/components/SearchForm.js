93b8b0c4d287389955a2f5d2a22d7783ff335942
c00kamunga <tsothalanti2@yahoo.com>
2020-03-20 17:42:41 -0700

    created searchForm to allow the user to filter through the list of characters that are being displayed

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -7,9 +7,19 @@ import styled from 'styled-components';
 
 export default function SearchForm(props) {
  const [search, setSearch] = useState('')
+ let list = props.character
+ console.log(list)
+ function nameFilter(event){
+   setSearch(event.target.value)
+   props.filter(search)
+ }
   return (
     <section className="search-form">
-     // Add a search form here
+     <form onSubmit={nameFilter}/>
+     <label>Search Name:</label>
+     <input type="text" placeholder="search for a name" onChange={nameFilter} required value={search}/>
+     <input type="submit" value="Find name"/>
+     <form/>
     </section>
   );
 }
