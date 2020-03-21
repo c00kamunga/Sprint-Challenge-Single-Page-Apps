061048cf78c334501b314a0465277d6738ebacfc
c00kamunga <tsothalanti2@yahoo.com>
2020-03-20 18:02:32 -0700

    added Route link components and render prop attributes for Welcome PAge component and CharacterList component, added stlying for both components, added Links to link between both components and styled as well

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,11 +1,19 @@
 import React from "react";
 
+const StyledIMG = styled.img`
+border: 10px solid red;
+dislay: block;
+margin-left: auto;
+auto;
+width: 50%;
+`
+
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
+        <StyledIMG
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
